8f3a1f527f5d152dbf7e3271bdaffb15c58dd32a
NANAMIyvonne <48374962+NANAMIyvonne@users.noreply.github.com>
2023-09-09 14:58:34 +1000

    Add files via upload

---
+++ generate_data/create_coldstart.sh
@@ -0,0 +1,5 @@
+for dataset in yeast human cora citeseer pubmed wordnet
+# for dataset in yeast
+do
+    taskset -c 28-47 python3 create_stream_coldstart_kmean.py --prefix dataspace/graph/${dataset}/${dataset}  --epochs 20 --pretrained_model  model_pt/${dataset}_sup_20.pt --save_model  model_pt/${dataset}_sup_20.pt  --cuda --num_subgraphs 20 --embedding_model ../data --rate 100
+done
